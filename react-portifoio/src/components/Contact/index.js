ae9d92cc20c90dff028df5c2b1cfc25ef0b43968
vikas kumar <vikas10145.12@bitmesra.ac.in>
2022-09-11 20:20:15 +0530

    home and contact page
    home and contact page

---
+++ react-portifoio/src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+
+const Contact =()=> {
+    return (
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'></div>
+            </div>
+            <Loader type="pacman" />
+        </>
+        
+    )
+}
