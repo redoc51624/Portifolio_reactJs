d763cbcb33ca8c5d28eb652b084fd135df7529ee
vikas kumar <vikas10145.12@bitmesra.ac.in>
2022-10-12 19:09:58 +0530

    firebase changes
    firebase changes

--- react-portifoio/src/components/Dashboard/home.js
+++ react-portifoio/src/components/Dashboard/home.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { auth, storage, db } from '../../firebase';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { addDoc } from 'firebase/firestore';
-import { collection } from 'firebase/firestore/lite';
+import { collection } from 'firebase/firestore';
 
 
 const Home = () => {
--- react-portifoio/src/firebase.js
+++ react-portifoio/src/firebase.js
@@ -1,24 +1,28 @@
 
-// import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-// import { getStorage } from 'firebase/storage';
-// import { getFirestore } from 'firebase/firestore/lite';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "",
-//   messagingSenderId: "",
-//   appId: ""
-// };
-
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth();
-// const provider = new GoogleAuthProvider();
-// export const db = getFirestore(app);
-// export const storage = getStorage(app);
+const firebaseConfig = {
+  apiKey: "AIzaSyA4XdPbIVAqTWyjqCokJziRB2-ngULl1nY",
+  authDomain: "react-portifolio-cb113.firebaseapp.com",
+  projectId: "react-portifolio-cb113",
+  storageBucket: "react-portifolio-cb113.appspot.com",
+  messagingSenderId: "783667859199",
+  appId: "1:783667859199:web:4a92e0b802d8b56e28c6ca",
+  measurementId: ''
+};
 
 
-// export const signInWithGoogle = () => signInWithPopup(auth, provider);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+const provider = new GoogleAuthProvider();
+export const db = getFirestore(app, {
+  experimentalForceLongPolling: true,
+});;
+export const storage = getStorage(app);
+// const analytics = getAnalytics(app);
+
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
